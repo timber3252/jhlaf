f7ede3554b68e4c051ea6ee018d9ec06a57e7ff5
Richard Acosta <timber3252@outlook.com>
2020-10-23 18:55:05 +0800

    fix: build dev & finish info.md

--- app.js
+++ app.js
@@ -1,33 +1,16 @@
-#!/usr/bin/node
+#!/bin/node
 
-const fs = require('fs');
-const https = require('https');
+var express = require('express')
+var bodyParser = require('body-parser');
+var app = express();
 
-const express = require('express');
-const vhost = require('vhost');
+app.use(bodyParser.json());         // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
-const options = {
-  cert: fs.readFileSync('cert.crt'),
-  key: fs.readFileSync('cert.key'),
-}
+app.use(express.static('public'));
 
-if (process.argv[2] === 'local') {
-  https.createServer(options, kanban).listen(20000);
-  console.log('kanban    --> https://localhost:20000');
-} else {
-  const app = express();
-  app.use(vhost('kanban.timber3252.me', kanban));
-  https.createServer(options, app).listen(443);
-
-  // redirect http to https
-  const http = express();
-  http.get('*', (req, res) => {
-    if (req.get('User-Agent') === 'Go-http-client/1.1') {
-      res.status(403).end('nmsl');
-    } else {
-      res.redirect('https://' + req.headers.host + req.url);
-    }
-  });
-  http.listen(80);
-  console.log('as always have a nice day!');
-}
+app.listen(3000, function () {
+  console.log('server was started on http://localhost:3000');
+});
