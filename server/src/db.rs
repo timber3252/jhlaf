6c69345d85bbdd67726c5027c5ebba09ace96bac
Richard Acosta <timber3252@outlook.com>
2020-10-25 01:24:55 +0800

    feat: backend update (db & reg)

--- server/src/db.rs
+++ server/src/db.rs
@@ -1,5 +1,56 @@
-use postgres::{Client, NoTls};
+use postgres::{NoTls, Client};
 
-mod db {
+pub fn check_database(args: &str) {
+  let s = String::from(args);
+  let ss = String::from(args) + " dbname=jhlaf";
+  let mut client = match Client::connect(&ss, NoTls) {
+    Ok(c) => c,
+    Err(e) => {
+      if e.code().unwrap().code() == "3D000" {
+        let mut c = Client::connect(&s, NoTls).unwrap();
+        c.batch_execute("CREATE DATABASE jhlaf").unwrap();
+        Client::connect(&ss, NoTls).unwrap()
+      } else {
+        panic!("{:?}", e);
+      }
+    }
+  };
+  if let Err(e) = client.batch_execute("
+    CREATE TABLE IF NOT EXISTS users (
+      id          SERIAL PRIMARY KEY,
+      username    TEXT NOT NULL,
+      password    TEXT NOT NULL,
+      userid      TEXT NOT NULL,
+      contact     TEXT NOT NULL,
+      group       TEXT NOT NULL
+    )
+  ") {
+    panic!("{:?}", e);
+  }
+  if let Err(e) = client.batch_execute("
+    CREATE TABLE IF NOT EXISTS items (
+      id          SERIAL PRIMARY KEY,
+      lof         BOOLEAN,
+      userid      TEXT NOT NULL,
+      type        TEXT NOT NULL,
+      name        TEXT NOT NULL,
+      image_url   TEXT NOT NULL,
+      desc        TEXT NOT NULL,
+      pickup_time TIMESTAMP WITHOUT TIME ZONE,
+      place       TEXT NOT NULL,
+      contact     TEXT NOT NULL,
+      post_time   TIMESTAMP WITHOUT TIME ZONE,
+      tags        TEXT NOT NULL
+    )
+  ") {
+    panic!("{:?}", e);
+  }
+}
 
+pub fn check_username(client: &mut Client, username: &str) -> bool {
+  let mut exist: bool = false;
+  for _ in client.query("SELECT * FROM users WHERE username = $1", &[&username]).unwrap() {
+    exist = true;
+  }
+  exist
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,79 @@
-mod db;
-mod wss;
+use json;
+use hmac::{Hmac, Mac, NewMac};
+use sha2::Sha256;
+use digest::Digest;
+use postgres::{NoTls, Client};
 
-fn handle_conn(stream: &mut tungstenite::WebSocket<std::net::TcpStream>) {
-  
+mod wss;
+mod db;
+
+// TODO: SAVE PICTURES
+
+fn result_json() -> String {
+  let mut res: String = String::new();
+  res
+}
+
+fn handle_conn(ws: &mut tungstenite::WebSocket<std::net::TcpStream>, dbarg: &str) {
+  let mut client = match Client::connect(dbarg, NoTls) {
+    Ok(c) => c,
+    Err(err) => {
+      ws.close(None);
+      return;
+    }
+  };
+  loop {
+    if let Ok(msg_raw) = ws.read_message() {
+      if let Ok(msg) = msg_raw.to_text() {
+        if let Ok(parsed) = json::parse(msg) {
+          if parsed["type"] == "register" {
+            let (username, password, contact) = (&parsed["username"], &parsed["password"], &parsed["contact"]);
+            if username.is_null() || password.is_null() || contact.is_null() {
+              continue;
+            }
+            if username.len() < 4 || username.len() > 32 || password.len() == 0 || contact.len() == 0 {
+              continue;
+            }
+            // TODO: Double check: make sure username doesn't have any other chars
+            
+            if db::check_username(&mut client, username.as_str().unwrap()) {
+              continue;
+            }
+            let uid = calc_userid(&username.dump(), &password.dump());
+          } else if parsed["type"] == "login" {
+
+          } else if parsed["type"] == "query_user" {
+
+          } else if parsed["type"] == "query_userdata" {
+
+          } else if parsed["type"] == "publish_lost" {
+
+          } else if parsed["type"] == "publish_found" {
+
+          } else if parsed["type"] == "delete_lost" {
+
+          } else if parsed["type"] == "delete_found" {
+
+          } else if parsed["type"] == "select" {
+
+          }
+        }
+      }
+    }
+  }
+}
+
+fn calc_userid(usr: &str, pwd: &str) -> String {
+  type HmacSha256 = Hmac<Sha256>;
+  if let Ok(mut mac) = HmacSha256::new_varkey(pwd.as_bytes()) {
+    mac.update(usr.as_bytes());
+    mac.update(pwd.as_bytes());
+    return format!("{:x}", Sha256::digest(&mac.finalize().into_bytes()));
+  }
+  String::from(pwd) + usr
 }
 
 fn main() {
+  db::check_database("host=localhost user=postgres");
   wss::ws_server("0.0.0.0:3001", handle_conn);
 }
