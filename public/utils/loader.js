8fc0cf08eec04650f0de210a996f75e74cb264f9
AllenZCH <w34399@126.com>
2020-10-27 23:02:18 +0800

    feat: update loader.js & wss.js

--- public/utils/loader.js
+++ public/utils/loader.js
@@ -1,60 +1,31 @@
 function page_initial(){
-    page_dict = {"homepage":null,"laf":null,"me":null,"login":null,"publish":null,"result":null};
+    page_dict = {"homepage":null,"laf":null,"me":null,"login":null,"publish":null,"result":null,"back":null};
+    current_page_id = "homepage";
 }
 
-function page_change(target_page_id) {
-    var current_page = document.getElementById("main_content").firstChild;
-    if(current_page!=null){
-        page_dict[current_page.id] = current_page;
-    }
-    if (current_page1=null){
-        setInterval.
-    }
-    if(page_dict.target_page_id!=null){
-        document.getElementById("main_content").replaceChild(page_dict[target_page_id],current_page);
+function page_update(target_page_id) {
+    var main = document.getElementById("main_content");
+    page_dict[current_page_id] = main.firstChild;
+    if(page_dict[target_page_id] != null){
+        main.replaceChild(page_dict[target_page_id],main.firstChild);
     }
     else{
-        page_render(target_page_id);
+        var temp = document.createElement("div");
+        fetch("********************************/"+target_page_id+".html")
+            .then(function(response){response.text().then(function(data){temp.innerHTML = data;});})
+        if(main.firstChild != null){
+            main.replaceChild(temp.firstChild,main.firstChild);
+        }
+        else{
+            main.prepend(temp.firstChild);
+        }
+        page_refresh(target_page_id);
     }
+    page_dict["back"] = current_page_id;
+    current_page_id = target_page_id;
 }
 
-function request_post(type){
-    var request_json={"type":type,"para1":arguments[1],"para2":arguments[2],"para3":arguments[3],"para4":arguments[4]}
-}
-
-function page_render(target_page_id){
-    fetch()
-}
-
-
-
-function add_post(json_obj,status){
-    var post_block=document.createElement("div");
-    var post_type=document.createElement("div");
-    var post_user_name=document.createElement("div");
-    var post_contact=document.createElement("div");
-    var post_status=d0cument.createElement("div");
-    post_type.innerHTML=json_obj.post_type;
-    post_user_name.innerHTML=json_obj.user.name;
-    post_contact.innerHTML=json_obj.user.contact;
-}
-function form_publish(){
-    var form_window=document.createElement("div");
-    var form=document.createElement("form")
-}
-
-
-
-function form_register(){
-    var form_window=document.createElement("div");
-    var form=document.createElement("form");
-    var studentID=document.createElement("input");
-    var password=document.createElement("input");
-}
-
-
-
-function add_post_(){
-
+function page_refresh(target_page_id){
+    
 }
 
