49973f522e9074e3f6905d1cfbee325ed98d2cee
Richard Acosta <timber3252@outlook.com>
2020-10-30 21:20:48 +0800

    fix: loader.js

--- public/utils/loader.js
+++ public/utils/loader.js
@@ -1,83 +1,104 @@
-function page_initial(){
-    page_dict = {"homepage" : null, "laf" : null, "me" : null, "login" : null, "publish" : null, "result" : null, };
-    back = null;
-    current_page_id = "login";
-    page_update("login");
+function page_initial() {
+  page_dict = {"homepage": null, "laf": null, "me": null, "login": null, "publish": null, "result": null, };
+  back = null;
+  current_page_id = null;
+  default_page = "homepage";
+  page_update(default_page);
+  // current_page_id = "login";
+  // page_update("login");
 }
 page_initial();
 
 function page_update(target_page_id) {
-    var main = document.getElementById("content");
-    var temp_child = main.firstChild;
-    if(page_dict[target_page_id] != null){
-        main.replaceChild(page_dict[target_page_id],main.firstChild);
-        if(target_page_id == "result"){
-            page_refresh("result", arguments[1]);
-        }
-    }
-    else{
-        var temp = document.createElement("div");
-        fetch('pages/'+target_page_id+'.html')
-            .then(function(response){response.text().then(function(data){temp.innerHTML = data;});})
-        if(main.firstChild != null){
-            main.replaceChild(temp.firstChild,main.firstChild);
-        }
-        else{
-            main.prepend(temp.firstChild);
-        }
-    }
-    page_dict[current_page_id] = temp_child;
-    if(current_page_id != target_page_id){
-        page_dict["back"] = temp_child;
-    }
-    current_page_id = target_page_id;
+  var content = document.querySelector('.content');
+  //   var temp_child = main.firstChild;
+  //   if (page_dict[target_page_id] != null) {
+  //     main.replaceChild(page_dict[target_page_id], main.firstChild);
+  //     if (target_page_id == "result") {
+  //       page_refresh("result", arguments[1]);
+  //     }
+  //   }
+  //   else {
+  //     var temp = document.createElement("div");
+  //     fetch('/pages/' + target_page_id + '.html')
+  //       .then(function (response) {response.text().then(function (data) {temp.innerHTML = data;});})
+  //     if (main.firstChild != null) {
+  //       main.replaceChild(temp.firstChild, main.firstChild);
+  //     }
+  //     else {
+  //       main.prepend(temp.firstChild);
+  //     }
+  //   }
+  //   page_dict[current_page_id] = temp_child;
+  //   if (current_page_id != target_page_id) {
+  //     page_dict["back"] = temp_child;
+  //   }
+  //   current_page_id = target_page_id;
+  if (current_page_id != null) {
+    page_dict[current_page_id] = content.innerHTML;
+  }
+  if (page_dict[target_page_id] == null) {
+    fetch('/pages/' + target_page_id + '.html')
+      .then(function (response) {
+        response.text().then(function (data) {
+          if (data.length > 0) {
+            content.innerHTML = data;
+          } else {
+            console.log('fetch error: data is null');
+          }
+        })
+      })
+  } else {
+    content.innerHTML = page_dict[target_page_id];
+  }
+  current_page_id = target_page_id;
 }
 
 
-function page_refresh(target_page_id){
-    switch(target_page_id){
-        case "homepage" : {
-            
-            break;
-        }
-        case "laf" : {
+function page_refresh(target_page_id) {
+  switch (target_page_id) {
+    case "homepage": {
 
-            break;
-        }
-        case "me" : {
-
-            break;
-        }
-        case "login" : {
-            
-            break;
-        }
-        case "publish" : {
-            
-            break;
-        }
-        case "result" : {
-            ws_send()
-            break;
-        }
+      break;
     }
+    case "laf": {
+
+      break;
+    }
+    case "me": {
+
+      break;
+    }
+    case "login": {
+
+      break;
+    }
+    case "publish": {
+
+      break;
+    }
+    case "result": {
+      ws_send()
+      break;
+    }
+  }
 }
 
 
 function toggleReg() {
-    document.getElementById("login_box").hidden = true;
-    document.getElementById("register_box").hidden = false;
+  document.getElementById("login_box").hidden = true;
+  document.getElementById("register_box").hidden = false;
 }
-  
+
 function toggleLogin() {
-    document.getElementById("register_box").hidden = true;
-    document.getElementById("login_box").hidden = false;
+  document.getElementById("register_box").hidden = true;
+  document.getElementById("login_box").hidden = false;
 }
 
 function login() {
-    username = document.getElementById("username1").value;
-    password = document.getElementById("password1").value;
-    ws_send("login", )
+  username = document.getElementById("username1").value;
+  password = document.getElementById("password1").value;
+  ws_send("login",)
 }
 //$('.post').on('click',page_update('result'))
 
