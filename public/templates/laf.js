3ad9d5c0c1608e579e9388e7feb2ef77cad3f241
OHaiYo <1286838355@qq.com>
2020-11-01 21:18:27 +0800

    feat: publish_found,rename publish_lost,fix: some classes and ids according to interact.md

--- public/templates/laf.js
+++ public/templates/laf.js
@@ -5,8 +5,19 @@ var example_template = '' +
     '</div>';
 
 
-var user_info = '' +
-    '<div class="laf1 inline">' +
+var lost_tab = '' +
+    '<div class="laf1 inline lost_tab">' +
+    ' <div>${image}</div>' +
+    '<div class="vertical">' +
+    '<div>${item_name}e</div>' +
+    '<div>${item_type}</div>' +
+    '<div>${publish_time}</div>' +
+    '</div>' +
+    '<div>${description}</div>' +
+    '</div>';
+
+var found_tab = '' +
+    '<div class="laf1 inline found_tab">' +
     ' <div>${image}</div>' +
     '<div class="vertical">' +
     '<div>${item_name}e</div>' +
